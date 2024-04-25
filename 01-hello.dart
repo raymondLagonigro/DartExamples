af7e3a3ff29bb14980cf291db441bf48e256993d
raymondLagonigro <36451649+raymondLagonigro@users.noreply.github.com>
2024-04-25 15:42:15 +0200

    Update 01-hello.dart

--- 01-hello.dart
+++ 01-hello.dart
@@ -1,3 +1,6 @@
+// Comprovar si Dart està instal·lat escrivint "dart --version" al terminal i comprovar la resposta
+//
+// si teniu el SDK de Dart instal·lat executar el programa: dart run hello.dart
 void main() {
   print('Hello, World!');
 }
